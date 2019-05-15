51d78775306d0145b03b5f46b913747d66b9c192
NohaIhab <49988746+NohaIhab@users.noreply.github.com>
2019-05-15 21:39:21 +0200

    Update CookieMonster.java

--- src/cookie/monster/CookieMonster.java
+++ src/cookie/monster/CookieMonster.java
@@ -1,49 +1,24 @@
 package cookie.monster;
 
-import java.nio.file.Paths;
 import javafx.application.Application;
 import javafx.stage.Stage;
 import static javafx.application.Application.launch;
-import javafx.geometry.Insets;
 import javafx.scene.Scene;
-import javafx.scene.control.Label;
-import javafx.scene.image.Image;
-import javafx.scene.image.ImageView;
-import javafx.scene.layout.Background;
-import javafx.scene.layout.BackgroundImage;
-import javafx.scene.layout.BackgroundPosition;
-import javafx.scene.layout.BackgroundRepeat;
-import javafx.scene.layout.BorderPane;
-import javafx.scene.media.AudioClip;
-import javafx.scene.paint.Color;
-import javafx.scene.text.Font;
-import javafx.scene.text.FontPosture;
-import javafx.scene.text.FontWeight;
 
 
 public class CookieMonster extends Application {
     
     @Override
     public void start(Stage primaryStage) {
-        BorderPane root = new BorderPane();
-        Label s = new Label("Press to Start");
-        AudioClip audio1 = new AudioClip(Paths.get("Sound\\HELLO.mp3").toUri().toString());
-       s.setFont(Font.font("Algerian", FontWeight.BOLD, FontPosture.ITALIC, 36));
-       s.setTextFill(Color.BURLYWOOD);
-       Image image = new Image("file:Pictures\\bg.jpeg");
-       ImageView i = new ImageView(image);
-       i.setFitHeight(510);
-       i.setFitWidth(510);
-       root.getChildren().add(i);
-       audio1.play();
-       
-       s.setOnMouseClicked(e->{
-           StartPane sp = new StartPane(500,500,primaryStage);
-       });
+        
        primaryStage.setResizable(false);
-       root.setTop(s);
-       BorderPane.setMargin(s,new Insets(350,0,350,180));
-       Scene scene = new Scene(root,500,500);
+ 
+       
+//       Stage charac=new Stage();
+//       CharactersPane noha=new CharactersPane();
+//       Scene s2=new Scene(noha,500,500);
+       StartPage spage=new StartPage(primaryStage,500,500);
+       Scene scene = new Scene(spage,500,500);
        primaryStage.setScene(scene);
        primaryStage.show();
        
