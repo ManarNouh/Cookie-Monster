ae635d459ccde2cb950b845c08e0346b74610bd0
NohaIhab <49988746+NohaIhab@users.noreply.github.com>
2019-05-15 22:10:18 +0200

    Update Player.java

--- src/cookie/monster/Player.java
+++ src/cookie/monster/Player.java
@@ -1,18 +1,21 @@
 package cookie.monster;
 
-import java.io.FileInputStream;
-import java.nio.file.Paths;
 import javafx.scene.image.Image;
-import javafx.scene.paint.ImagePattern;
-import javafx.scene.shape.Circle;
+import javafx.scene.image.ImageView;
 
 
-class Player extends Circle { 
+class Player extends ImageView { 
     private boolean state = true;
+ 
+    private Image CM=new Image("file:Pictures\\s-l300.png");
+ 
     
     public Player( double Wlim, double Hlim) {
-        super(Wlim/2,Hlim - Hlim/8,50);
-        this.setFill(CM());
+        this.setImage(CM);
+        setFitWidth(100);
+        setFitHeight(100);
+        setX(Wlim/2);
+        setY(Hlim-Hlim/4);
         this.setOnKeyPressed(
                 e -> {
                     double newX = getBoundsInParent().getMaxX();
@@ -21,14 +24,19 @@ class Player extends Circle {
                     switch(e.getCode()) {
                         case RIGHT: {
                         if (newX < Wlim) {
-                           setCenterX(getCenterX() + 25);} 
-                        else setCenterX(getCenterX() - 1);break;}
+                           setX(getX() + 25);} 
+                        else setX(getX() - 1);break;}
                         case LEFT: {
                        if (newX1 > 0) {
-                            setCenterX(getCenterX() - 25); }
-                    else setCenterX(getCenterX() + 1); break;}}
+                            setX(getX() - 25); }
+                    else setX(getX() + 1); break;}}
         });
+        
+      
+        
+     
     }
+    
         
     public boolean isAlive(){
         return state;
@@ -38,12 +46,6 @@ class Player extends Circle {
         this.state = state;
     }
     
-    private ImagePattern  CM() {
-       
-        Image image1 = new Image("file:Pictures\\s-l300.png");
-            ImagePattern imagePattern = new ImagePattern(image1);
-            return imagePattern;
-    }
+   
+   
 }
-
-
