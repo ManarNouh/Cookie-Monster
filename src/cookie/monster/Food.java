0a8f8fe30917d60cf7a03ef433b1de3fca6a3980
NohaIhab <49988746+NohaIhab@users.noreply.github.com>
2019-05-16 20:00:19 +0200

    Update Food.java

--- src/cookie/monster/Food.java
+++ src/cookie/monster/Food.java
@@ -4,8 +4,25 @@ import javafx.scene.image.Image;
 
 
 class Food extends GameObject {  
-    public Food(int r,double Hlim) {
+  private final Image elmoFood=new Image("file:Pictures\\apple.png");
+  private final Image birdFood=new Image("file:Pictures\\burger.png");
+  private final Image abbyFood=new Image("file:Pictures\\cupcake.png");
+  private final Image cmFood=new Image("file:Pictures\\ck.png");
+  private final Image[] fimgs=new Image[4];
+  
+    public Food(int r,double Hlim,final int x) {
         super(r,Hlim,new Image("file:Pictures\\ck.png"));
+        
+        fimgs[0]=cmFood;
+        fimgs[1]=elmoFood;
+        fimgs[2]=birdFood;
+        fimgs[3]=abbyFood;
+        
+        this.setImage(fimgs[x]);
+        this.setFitHeight(70);
+        if (x==2) setFitWidth(85);
+        this.setFitWidth(70);
+
     }  
     
 }
